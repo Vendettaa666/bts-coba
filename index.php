5ace84fddde3bec8b37e976221bfb56af46a74fb
Vendettaa666 <vinnfamily666@gmail.com>
2025-04-22 15:01:19 +0700

    add header

--- index.php
+++ index.php
@@ -3,9 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="resource/css/style.css">  
     <title>Document</title>
 </head>
 <body>
-    <h1>Hello World</h1>
+    <header>
+        <div class="logo-container">
+            <img src="assets/img/logosmk.png" alt="ulaancok">
+            <a href="#">Buku Tahunan Siswa</a>
+        </div>
+        
+        <div class="button-container">
+            <button class="download-button">Download</button>
+        </div>
+    </header>
+    <main>
+    </main>
+    <footer>Copyright &copy; 2025</footer>
 </body>
 </html>
