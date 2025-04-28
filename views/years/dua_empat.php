7bbe46e39e60721e4f0751700f7f4dcd94f3edf6
vendettaa666 <vinnfamily666@gmail.com>
2025-04-28 23:43:28 +0700

    menambah buku di 2024

--- views/years/dua_empat.php
+++ views/years/dua_empat.php
@@ -3,12 +3,81 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../../resource/css/dua_empat.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.2.0/remixicon.min.css">
     <title>Document</title>
 </head>
 <body>
-    <header>
-
+    <header class="header">
+        <div class="header-content">
+            <img src="../../assets/img/logosmk.png" alt="">
+            <p>Buku Tahunan Siswa</p>
+        </div>
+        <button class="Download-button">Download</button>
     </header>
+    <main> 
+        <section class="content">
+            <div class="content-header">
+                <div class="youtube-video">
+                <iframe width="590" height="390" src="https://www.youtube.com/embed/9uVRyso4y0g?si=0w8XgVHcV9t6us9w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </div>
+                <div class="layout-content">
+                    <button class="layout-button">
+                        <p>KOMITE SMK</p>
+                    </button>
+                    <button class="layout-button">
+                        <p>Tenaga Pendidik</p>
+                    </button>
+                    <button class="layout-button">
+                        <p>Wakil Kepala & Kepala Tata Usaha</p>
+                    </button>
+                    <button class="layout-button">
+                        <p>Tenaga Kependidikan</p>
+                    </button>
+                    <button class="layout-button">
+                        <p>Ketua Program Keahlian</p>
+                    </button>
+                    <button class="layout-button">
+                        <p>POKJA</p>
+                    </button>
+                    <button class="layout-button">
+                        <p>OSIS sayang</p>
+                        <i class="ri-arrow-right-long-line"></i>
+                    </button>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="buku-kelas">
+                <p>SISWA DAN SISWI</p>
+                <div class="buku-kelas-content">
+                    <img src="../../assets/buku-perkelas/akuntansi3.png" alt="">
+                    <h1>judul</h1>
+                    <h2>Subjudul</h2>
+                    <hr>    
+                    <button>Lihat selengkapnya</button>
+                </div>
+                <div class="buku-kelas-content">
+                    <img src="../../assets/buku-perkelas/manajemen-perkantoran2.png" alt="">
+                    <h1>judul</h1>
+                    <h2>Subjudul</h2>
+                    <hr>    
+                    <button>Lihat selengkapnya</button>
+                </div>
+                <div class="buku-kelas-content">
+                    <img src="../../assets/buku-perkelas/rpl-11.png" alt="">
+                    <h1>judul</h1>
+                    <h2>Subjudul</h2>
+                    <hr>    
+                    <button>Lihat selengkapnya</button>
+                </div>
+            </div>
+
+            <!-- <div class="buku-kelas">
+                <p>LAINNYA</p>
+            </div> -->
+        </section>
+    </main>
 </body>
 </html>
