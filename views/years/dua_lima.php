bfce272f64d5f1c3423b646aa0e3f8df8be91558
SaqisCode <satrianuqitaufani@gmail.com>
2025-04-29 14:56:05 +0700

    update video layer

--- views/years/dua_lima.php
+++ views/years/dua_lima.php
@@ -1,14 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../../resource/css/dua_lima.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.2.0/remixicon.min.css">
     <title>Document</title>
 </head>
-<body>
-    <header>
 
+<body>
+    <header class="header">
+        <div class="header-content">
+            <img src="../../assets/img/logosmk.png" alt="">
+            <p>Buku Tahunan Siswa</p>
+        </div>
+        <button class="Download-button">Download</button>
     </header>
+    <main>
+        <section class="content">
+            <div class="video-container">
+                <div class="custom-video-frame">
+                    <img src="../../assets/img/border.png" class="frame-image" alt="Video Frame">
+                    <div class="youtube-video">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/IUqF6cAKR6Q?si=HBdTR8qiECEJW-E-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+            <div class="layout-container">
+                <button class="layout-button">
+                    <p>KOMITE SMK</p>
+                </button>
+                <button class="layout-button">
+                    <p>Tenaga Pendidik</p>
+                </button>
+                <button class="layout-button">
+                    <p>Wakil Kepala & Kepala Tata Usaha</p>
+                </button>
+                <button class="layout-button">
+                    <p>Tenaga Kependidikan</p>
+                </button>
+                <button class="layout-button">
+                    <p>Ketua Program Keahlian</p>
+                </button>
+                <button class="layout-button">
+                    <p>POKJA</p>
+                </button>
+                <button class="layout-button">
+                    <p>TOHA YULI OSIS</p>
+                </button>
+            </div>
+        </section>
+    </main>
 </body>
+
 </html>
