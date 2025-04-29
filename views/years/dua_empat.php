b320d9229ef510b7bede6017df79b759542cb7d0
Vendettaa666 <vinnfamily666@gmail.com>
2025-04-29 14:53:33 +0700

    add-layout-book

--- views/years/dua_empat.php
+++ views/years/dua_empat.php
@@ -22,50 +22,57 @@
                 <iframe width="590" height="390" src="https://www.youtube.com/embed/9uVRyso4y0g?si=0w8XgVHcV9t6us9w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                 </div>
                 <div class="layout-content">
-                    <button class="layout-button">
+                    <div class="layout-button">
                         <p>KOMITE SMK</p>
-                    </button>
-                    <button class="layout-button">
+                    </div>
+                    <div class="layout-button">
                         <p>Tenaga Pendidik</p>
-                    </button>
-                    <button class="layout-button">
+                    </div>
+                    <div class="layout-button">
                         <p>Wakil Kepala & Kepala Tata Usaha</p>
-                    </button>
-                    <button class="layout-button">
+                    </div>
+                    <div class="layout-button">
                         <p>Tenaga Kependidikan</p>
-                    </button>
-                    <button class="layout-button">
+                    </div>
+                    <div class="layout-button">
                         <p>Ketua Program Keahlian</p>
-                    </button>
-                    <button class="layout-button">
+                    </div>
+                    <div class="layout-button">
                         <p>POKJA</p>
-                    </button>
-                    <button class="layout-button">
+                    </div>
+                    <div class="layout-button-osis">
                         <p>OSIS sayang</p>
-                        <i class="ri-arrow-right-long-line"></i>
-                    </button>
+                    </div>
                 </div>
             </div>
         </section>
 
-        <section>
+        <section class="content-book">
+        <p>SISWA DAN SISWI</p>
             <div class="buku-kelas">
-                <p>SISWA DAN SISWI</p>
-                <div class="buku-kelas-content">
+                <div class="buku-kelas-card">
                     <img src="../../assets/buku-perkelas/akuntansi3.png" alt="">
                     <h1>judul</h1>
                     <h2>Subjudul</h2>
                     <hr>    
                     <button>Lihat selengkapnya</button>
                 </div>
-                <div class="buku-kelas-content">
+                <div class="buku-kelas-card">
                     <img src="../../assets/buku-perkelas/manajemen-perkantoran2.png" alt="">
                     <h1>judul</h1>
                     <h2>Subjudul</h2>
                     <hr>    
                     <button>Lihat selengkapnya</button>
                 </div>
-                <div class="buku-kelas-content">
+                <div class="buku-kelas-card">
+                    <img src="../../assets/buku-perkelas/rpl-11.png" alt="">
+                    <h1>judul</h1>
+                    <h2>Subjudul</h2>
+                    <hr>    
+                    <button>Lihat selengkapnya</button>
+                </div>
+                
+                <div class="buku-kelas-card">
                     <img src="../../assets/buku-perkelas/rpl-11.png" alt="">
                     <h1>judul</h1>
                     <h2>Subjudul</h2>
@@ -73,11 +80,43 @@
                     <button>Lihat selengkapnya</button>
                 </div>
             </div>
-
-            <!-- <div class="buku-kelas">
-                <p>LAINNYA</p>
-            </div> -->
+        </section>
+        <section class="content-book">
+        <p>SISWA DAN SISWI</p>
+            <div class="buku-kelas">
+                <div class="buku-kelas-card">
+                    <img src="../../assets/buku-perkelas/akuntansi3.png" alt="">
+                    <h1>judul</h1>
+                    <h2>Subjudul</h2>
+                    <hr>    
+                    <button>Lihat selengkapnya</button>
+                </div>
+                <div class="buku-kelas-card">
+                    <img src="../../assets/buku-perkelas/manajemen-perkantoran2.png" alt="">
+                    <h1>judul</h1>
+                    <h2>Subjudul</h2>
+                    <hr>    
+                    <button>Lihat selengkapnya</button>
+                </div>
+                <div class="buku-kelas-card">
+                    <img src="../../assets/buku-perkelas/akuntansi3.png" alt="">
+                    <h1>judul</h1>
+                    <h2>Subjudul</h2>
+                    <hr>    
+                    <button>Lihat selengkapnya</button>
+                </div>
+                <div class="buku-kelas-card">
+                    <img src="../../assets/buku-perkelas/manajemen-perkantoran2.png" alt="">
+                    <h1>judul</h1>
+                    <h2>Subjudul</h2>
+                    <hr>    
+                    <button>Lihat selengkapnya</button>
+                </div>
         </section>
     </main>
+    <footer>
+
+    
+    </footer>
 </body>
 </html>
